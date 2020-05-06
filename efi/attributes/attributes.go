a442338442c1b8285f0f20b124373bd5e5d706c0
Morten Linderud <morten@linderud.pw>
2020-05-06 23:42:13 +0200

    [attributes] Define the attributes and add comments
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- efi/attributes/attributes.go
+++ efi/attributes/attributes.go
@@ -5,19 +5,30 @@ import (
 	"fmt"
 	"os"
 	"path"
+
+	"github.com/foxboron/goefi/efi/util"
 )
 
+// Section 8.2 Variable Services
 type Attributes uint32
 
 const (
 	EFI_VARIABLE_NON_VOLATILE                          Attributes = 0x00000001
-	EFI_VARIABLE_BOOTSERVICE_ACCESS                               = 0x00000002
-	EFI_VARIABLE_RUNTIME_ACCESS                                   = 0x00000004
-	EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS            = 0x00000020
-	EFI_VARIABLE_APPEND_WRITE                                     = 0x00000040
+	EFI_VARIABLE_BOOTSERVICE_ACCESS                    Attributes = 0x00000002
+	EFI_VARIABLE_RUNTIME_ACCESS                        Attributes = 0x00000004
+	EFI_VARIABLE_HARDWARE_ERROR_RECORD                 Attributes = 0x00000008
+	EFI_VARIABLE_AUTHENTICATED_WRITE_ACCESS            Attributes = 0x00000010 // Deprecated, we only reserve it
+	EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS Attributes = 0x00000020
+	EFI_VARIABLE_APPEND_WRITE                          Attributes = 0x00000040
+	EFI_VARIABLE_ENHANCED_AUTHENTICATED_ACCESS         Attributes = 0x00000080 // Uses the EFI_VARIABLE_AUTHENTICATION_3 struct
 )
 
-const GUID = "8be4df61-93ca-11d2-aa0d-00e098032b8c"
+// NV -> Non-Volatile
+// BS -> Boot Services
+// RT -> Runtime Services
+// AT -> Time Based Authenticated Write Access
+
+var EFI_GLOBAL_VARIABLE = util.EFIGUID{0x8BE4DF61, 0x93CA, 0x11d2, [8]uint8{0xAA, 0x0D, 0x00, 0xE0, 0x98, 0x03, 0x2B, 0x8C}}
 
 var Efivars = "/sys/firmware/efi/efivars"
 
@@ -44,7 +55,7 @@ func ParseEfivars(f *os.File) (*EfiVariable, error) {
 }
 
 func ReadEfivars(filename string) (*EfiVariable, error) {
-	f, err := os.Open(path.Join(Efivars, fmt.Sprintf("%s-%s", filename, GUID)))
+	f, err := os.Open(path.Join(Efivars, fmt.Sprintf("%s-%s", filename, EFI_GLOBAL_VARIABLE.Format())))
 	if err != nil {
 		return &EfiVariable{}, nil
 	}
