b1eea7b57a913e2f2bd43a08ba19222007cbaba8
Morten Linderud <morten@linderud.pw>
2020-05-10 14:05:11 +0200

    Some fixups to the signature package
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- efi/signature/signature.go
+++ efi/signature/signature.go
@@ -1,4 +1,4 @@
-package secureboot
+package signature
 
 import (
 	"bytes"
--- efi/signature/signature_test.go
+++ efi/signature/signature_test.go
@@ -1,4 +1,4 @@
-package secureboot
+package signature
 
 import (
 	"bytes"
@@ -11,7 +11,7 @@ import (
 )
 
 func TestParseSignatureList(t *testing.T) {
-	dir := "../../tests/data/keys/pk"
+	dir := "../../tests/data/signatures/efivars"
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		log.Fatal(err)
@@ -30,32 +30,39 @@ func TestParseSignatureList(t *testing.T) {
 
 		f := bytes.NewReader(s.Data)
 		ReadSignatureList(f)
-		break
 	}
 }
 
 func TestParseSignatureSupport(t *testing.T) {
-	file := "../../tests/data/signature/SignatureSupport-8be4df61-93ca-11d2-aa0d-00e098032b8c"
-	s, _ := attributes.ReadEfivarsFile(file)
+	dir := "../../tests/data/signatures/sigsupport"
 
-	var pkflags attributes.Attributes
-	pkflags |= attributes.EFI_VARIABLE_BOOTSERVICE_ACCESS
-	pkflags |= attributes.EFI_VARIABLE_RUNTIME_ACCESS
-	if (pkflags & s.Attributes) != pkflags {
-		t.Errorf("Incorrect bitmask")
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
 	}
-	f := bytes.NewReader(s.Data)
-	guids := GetSupportedSignatures(f)
-	if guids[0] != CERT_SHA1_GUID {
-		t.Errorf("Unexpected certificate, should be SHA1")
-	}
-	if guids[1] != CERT_SHA256_GUID {
-		t.Errorf("Unexpected certificate, should be SHA256")
-	}
-	if guids[2] != CERT_RSA2048_GUID {
-		t.Errorf("Unexpected certificate, should be RSA2048")
-	}
-	if guids[3] != CERT_X509_GUID {
-		t.Errorf("Unexpected certificate, should be X509")
+	for _, file := range files {
+		path := filepath.Join(dir, file.Name())
+		s, _ := attributes.ReadEfivarsFile(path)
+
+		var pkflags attributes.Attributes
+		pkflags |= attributes.EFI_VARIABLE_BOOTSERVICE_ACCESS
+		pkflags |= attributes.EFI_VARIABLE_RUNTIME_ACCESS
+		if (pkflags & s.Attributes) != pkflags {
+			t.Errorf("Incorrect bitmask")
+		}
+		f := bytes.NewReader(s.Data)
+		guids := GetSupportedSignatures(f)
+		if guids[0] != CERT_SHA1_GUID {
+			t.Errorf("Unexpected certificate, should be SHA1")
+		}
+		if guids[1] != CERT_SHA256_GUID {
+			t.Errorf("Unexpected certificate, should be SHA256")
+		}
+		if guids[2] != CERT_RSA2048_GUID {
+			t.Errorf("Unexpected certificate, should be RSA2048")
+		}
+		if guids[3] != CERT_X509_GUID {
+			t.Errorf("Unexpected certificate, should be X509")
+		}
 	}
 }
