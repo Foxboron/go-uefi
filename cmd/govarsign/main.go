7c0c408f27f549fd5716b7e86267210d267d63d2
Morten Linderud <morten@linderud.pw>
2020-05-23 17:15:18 +0200

    Refactored cert parsing to util
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- cmd/govarsign/main.go
+++ cmd/govarsign/main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"bytes"
-	"crypto/rsa"
-	"crypto/x509"
-	"encoding/pem"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -15,44 +12,6 @@ import (
 	"github.com/foxboron/goefi/efi/attributes"
 	"github.com/foxboron/goefi/efi/signature"
 )
-
-func ReadKey(path string) *rsa.PrivateKey {
-	b, err := ioutil.ReadFile(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-	block, _ := pem.Decode(b)
-	if block == nil {
-		panic("failed to parsePEM block containg the public key!")
-	}
-	priv, err := x509.ParsePKCS8PrivateKey(block.Bytes)
-	if err != nil {
-		panic("failed to parse DER encoded private key: " + err.Error())
-	}
-	switch priv := priv.(type) {
-	case *rsa.PrivateKey:
-		return priv
-	default:
-		panic("unknown type of public key")
-	}
-}
-
-func ReadCert(path string) *x509.Certificate {
-	b, err := ioutil.ReadFile(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-	block, _ := pem.Decode(b)
-	if block == nil {
-		panic("failed to parsePEM block containg the public key!")
-	}
-
-	cert, err := x509.ParseCertificate(block.Bytes)
-	if err != nil {
-		panic("failed to parse certificate: " + err.Error())
-	}
-	return cert
-}
 
 func main() {
 	key := flag.String("key", "", "Key")
@@ -76,8 +35,8 @@ func main() {
 	attrs |= attributes.EFI_VARIABLE_APPEND_WRITE
 
 	ctx := &signature.SigningContext{
-		Cert:    ReadCert(*cert),
-		Key:     ReadKey(*key),
+		Cert:    util.ReadCert(*cert),
+		Key:     util.ReadKey(*key),
 		Varname: []byte(*variable),
 		Guid:    attributes.EFI_GLOBAL_VARIABLE,
 		Attr:    attrs,
---
+++ efi/util/certs.go
@@ -0,0 +1,47 @@
+package util
+
+import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"io/ioutil"
+	"log"
+)
+
+func ReadKey(path string) *rsa.PrivateKey {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	block, _ := pem.Decode(b)
+	if block == nil {
+		panic("failed to parsePEM block containg the public key!")
+	}
+	priv, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		panic("failed to parse DER encoded private key: " + err.Error())
+	}
+	switch priv := priv.(type) {
+	case *rsa.PrivateKey:
+		return priv
+	default:
+		panic("unknown type of public key")
+	}
+}
+
+func ReadCert(path string) *x509.Certificate {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	block, _ := pem.Decode(b)
+	if block == nil {
+		panic("failed to parsePEM block containg the public key!")
+	}
+
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		panic("failed to parse certificate: " + err.Error())
+	}
+	return cert
+}
