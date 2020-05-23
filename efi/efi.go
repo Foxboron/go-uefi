7e309eb7c4f6ce2979edc592b75a34a565cb3760
Morten Linderud <morten@linderud.pw>
2020-05-23 17:16:17 +0200

    Add GetSetupMode and GetSecureBoot
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- efi/efi.go
+++ efi/efi.go
@@ -49,9 +49,24 @@ func GetBootEntry(entry string) *device.EFILoadOption {
 	return loadOption
 }
 
-func GetSetupMode() []byte {
-	attributes.ReadEfivars("SetupMode")
-	return []byte{}
+// GetSetupMode returns if setup mode has been enabled on the machine.
+func GetSetupMode() bool {
+	if sm, err := attributes.ReadEfivars("SetupMode"); err == nil {
+		if sm.Data[0] == 1 {
+			return true
+		}
+	}
+	return false
+}
+
+// GetSecureBoot returns if secure boot has been enabled on the machine.
+func GetSecureBoot() bool {
+	if sm, err := attributes.ReadEfivars("SecureBoot"); err == nil {
+		if sm.Data[0] == 1 {
+			return true
+		}
+	}
+	return false
 }
 
 func GetPK() error {
