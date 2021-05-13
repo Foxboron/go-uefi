913dbe1d0398f849f5078f066278fb5c76f8672e
Morten Linderud <morten@linderud.pw>
2021-05-13 13:19:02 +0200

    Added GetSignatures
    Signed-off-by: Morten Linderud <morten@linderud.pw>

---
+++ efi/pecoff/reader.go
@@ -0,0 +1,36 @@
+package pecoff
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/foxboron/go-uefi/efi/signature"
+	"github.com/pkg/errors"
+)
+
+func GetSignatures(pefile []byte) ([]*signature.WINCertificate, error) {
+	var signatures []*signature.WINCertificate
+
+	sigbuf, err := GetSignatureBytesFromFile(pefile)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to fetch signatures slice")
+	}
+
+	reader := bytes.NewReader(sigbuf)
+	for reader.Len() > signature.SizeofWINCertificate {
+		sig, err := signature.ReadWinCertificate(reader)
+		if err != nil {
+			return []*signature.WINCertificate{}, errors.Wrap(err, "")
+		}
+		signatures = append(signatures, &sig)
+
+		// All wCerts are padded up to 8 bytes
+		// this figures out the padding and removes the bytes
+		// so we don't parse them.
+		_, size := PaddingBytes(int(sig.Length), 8)
+		fmt.Println(size)
+		reader.Read(make([]byte, size))
+	}
+
+	return signatures, nil
+}
