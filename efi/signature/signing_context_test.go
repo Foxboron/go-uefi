634ac081ebfdb7ecf93be0377f8c8901e5168230
Morten Linderud <morten@linderud.pw>
2021-06-03 20:21:26 +0200

    Removed unused function
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- efi/signature/signing_context_test.go
+++ efi/signature/signing_context_test.go
@@ -2,29 +2,10 @@ package signature
 
 import (
 	"bytes"
-	"crypto/x509"
-	"encoding/pem"
 	"io/ioutil"
 	"log"
 	"testing"
 )
-
-func ReadCert(path string) *x509.Certificate {
-	b, err := ioutil.ReadFile(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-	block, _ := pem.Decode(b)
-	if block == nil {
-		panic("failed to parsePEM block containing the public key!")
-	}
-
-	cert, err := x509.ParseCertificate(block.Bytes)
-	if err != nil {
-		panic("failed to parse certificate: " + err.Error())
-	}
-	return cert
-}
 
 // Doesn't do anything
 func TestVerifySignature(t *testing.T) {
