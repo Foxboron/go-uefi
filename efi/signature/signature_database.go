cb55187f1986569221b019f1826e70a0fab13362
Morten Linderud <morten@linderud.pw>
2021-06-29 19:13:53 +0200

    signature_database: Implement deletion

--- efi/signature/signature_database.go
+++ efi/signature/signature_database.go
@@ -2,6 +2,7 @@ package signature
 
 import (
 	"io"
+	"reflect"
 
 	"github.com/foxboron/go-uefi/efi/util"
 	"github.com/pkg/errors"
@@ -9,6 +10,8 @@ import (
 
 // SignatureDatabase is a list of EFI signature lists
 type SignatureDatabase []*SignatureList
+
+var ErrNotFoundSigList = errors.New("signature list not found")
 
 // Checks if all signatures in a list is present in the signature database
 func (sd *SignatureDatabase) SigDataExists(certtype util.EFIGUID, sigdata *SignatureData) bool {
@@ -60,6 +63,58 @@ func (sd *SignatureDatabase) AppendSignature(certtype util.EFIGUID, sl *Signatur
 // TODO: Should merge towards a fitting list?
 func (sd *SignatureDatabase) AppendList(sl *SignatureList) {
 	*sd = append(*sd, sl)
+}
+
+// Remove the raw signature values to the database
+func (sd *SignatureDatabase) Remove(certtype util.EFIGUID, owner util.EFIGUID, data []byte) error {
+	// We might want to differentiate between not finding a list for the type and
+	// not finding appropriate sigdata in the different lists.
+	nosigdata := false
+	for _, l := range *sd {
+		if !util.CmpEFIGUID(l.SignatureType, certtype) {
+			continue
+		}
+		size := uint32(len(data)) + util.SizeofEFIGUID
+		if size != l.Size {
+			continue
+		}
+		if err := l.RemoveBytes(owner, data); errors.Is(err, ErrNotFoundSigData) {
+			nosigdata = true
+			continue
+		}
+		if len(l.Signatures) == 0 {
+			return sd.RemoveList(l)
+		}
+		return nil
+	}
+	if nosigdata {
+		return ErrNotFoundSigData
+	}
+	return ErrNotFoundSigList
+}
+
+// Removes a signaure to the database. It will scan available lists for something to remove
+func (sd *SignatureDatabase) RemoveSignature(certtype util.EFIGUID, sl *SignatureData) error {
+	return sd.Remove(certtype, sl.Owner, sl.Data)
+}
+
+func (sd *SignatureDatabase) removeslice(index int) {
+	if len(*sd) == 1 {
+		*sd = SignatureDatabase{}
+		return
+	}
+	*sd = append((*sd)[:index], (*sd)[index+1:]...)
+}
+
+// Removes a signature list from the database
+func (sd *SignatureDatabase) RemoveList(sl *SignatureList) error {
+	for index, siglist := range *sd {
+		if reflect.DeepEqual(sl, siglist) {
+			sd.removeslice(index)
+			return nil
+		}
+	}
+	return ErrNotFoundSigList
 }
 
 // Write a signature database which contains a slice of SignautureLists
--- efi/signature/signature_database_test.go
+++ efi/signature/signature_database_test.go
@@ -2,24 +2,103 @@ package signature
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
+
+	"github.com/pkg/errors"
 )
 
-func TestSigdatabase(t *testing.T) {
+func TestSigdatabaseAppend(t *testing.T) {
 	sigdb := SignatureDatabase{}
 	for _, sig := range sigdata {
 		sigdb.AppendSignature(CERT_SHA256_GUID, &sig)
 	}
 	if sigdb[0].ListSize != 172 {
 		fmt.Println(sigdb[0].ListSize)
-		t.Fatal("list size incorrect")
+		t.Fatal("append: list size incorrect")
 	}
 	if sigdb[0].Size != 48 {
 		fmt.Println(sigdb[0].ListSize)
-		t.Fatal("size incorrect")
+		t.Fatal("append: size incorrect")
 	}
 	if len(sigdb[0].Signatures) != 3 {
-		t.Fatal("number of signatures wrong")
+		t.Fatal("append: number of signatures wrong")
+	}
+}
+
+func TestSigdatabaseAppendSame(t *testing.T) {
+	sigdb := SignatureDatabase{}
+	for _, sig := range sigdata {
+		sigdb.AppendSignature(CERT_SHA256_GUID, &sig)
+	}
+	sigdb.AppendSignature(CERT_SHA256_GUID, &sigdata[0])
+	if len(sigdb[0].Signatures) != 3 {
+		t.Fatal("append: number of signatures wrong")
+	}
+}
+
+func TestSigdatabaseRemove(t *testing.T) {
+	sigdb := SignatureDatabase{}
+	for _, sig := range sigdata {
+		sigdb.AppendSignature(CERT_SHA256_GUID, &sig)
+	}
+	err := sigdb.RemoveSignature(CERT_SHA256_GUID, &sigdata[0])
+	if err != nil {
+		t.Error(err)
+	}
+	if sigdb[0].ListSize != 124 {
+		fmt.Println(sigdb[0].ListSize)
+		t.Fatal("remove: list size incorrect")
+	}
+	if sigdb[0].Size != 48 {
+		fmt.Println(sigdb[0].ListSize)
+		t.Fatal("remove: size incorrect")
+	}
+	if len(sigdb[0].Signatures) != 2 {
+		t.Fatal("remove: number of signatures wrong")
+	}
+}
+
+func TestSigdatabaseRemoveSame(t *testing.T) {
+	sigdb := SignatureDatabase{}
+	for _, sig := range sigdata {
+		sigdb.AppendSignature(CERT_SHA256_GUID, &sig)
+	}
+	sigdb.RemoveSignature(CERT_SHA256_GUID, &sigdata[0])
+	err := sigdb.RemoveSignature(CERT_SHA256_GUID, &sigdata[0])
+	if !errors.Is(err, ErrNotFoundSigData) {
+		t.Fatalf("remove: wrong error, got: %s", err)
+	}
+}
+
+func TestSigdatabaseRemoveWrongSiglistType(t *testing.T) {
+	sigdb := SignatureDatabase{}
+	for _, sig := range sigdata {
+		sigdb.AppendSignature(CERT_SHA256_GUID, &sig)
+	}
+	err := sigdb.RemoveSignature(CERT_X509_GUID, &sigdata[0])
+	if !errors.Is(err, ErrNotFoundSigList) {
+		t.Fatalf("remove: wrong error, got: %s", err)
+	}
+}
+
+func TestSigdatabaseRemoveAll(t *testing.T) {
+	var err error
+	sigdb := SignatureDatabase{}
+	for _, sig := range sigdata {
+		sigdb.AppendSignature(CERT_SHA256_GUID, &sig)
+	}
+	for index, sig := range sigdata {
+		err = sigdb.RemoveSignature(CERT_SHA256_GUID, &sig)
+		if len(sigdb) != 0 && 3-(index+1) != len(sigdb[0].Signatures) {
+			t.Fatalf("remove all: sigdb wrong size. expected %d, got %d", 3-(index+1), len(sigdb[0].Signatures))
+		}
+	}
+	if err != nil {
+		t.Fatalf("remove all: got error: %s", err)
+	}
+	if !reflect.DeepEqual(sigdb, SignatureDatabase{}) {
+		t.Fatalf("remove all: not empty interface")
 	}
 }
 
