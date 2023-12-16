7ae6965daeb7d404e12d155ca9aaee759a1f94c9
Morten Linderud <morten@linderud.pw>
2023-12-16 15:52:41 +0100

    asntest: helper functions to diff asn
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- asntest/asntest.go
+++ asntest/asntest.go
@@ -1,3 +1,4 @@
+// Package for helper function in the test suite
 package asntest
 
 import (
@@ -5,10 +6,16 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"crypto/x509/pkix"
+	"fmt"
 	"log"
 	"math/big"
+	"os"
+	"os/exec"
+	"path"
+	"testing"
 )
 
+// Init a basic set of certs
 func InitCert() (*x509.Certificate, *rsa.PrivateKey) {
 	serialNumber, err := rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), 128))
 	if err != nil {
@@ -31,4 +38,34 @@ func InitCert() (*x509.Certificate, *rsa.PrivateKey) {
 	derBytes, _ := x509.CreateCertificate(rand.Reader, &c, &c, &priv.PublicKey, priv)
 	cert, _ := x509.ParseCertificate(derBytes)
 	return cert, priv
+}
+
+func Asn1Parse(t *testing.T, buf []byte) []byte {
+	f := path.Join(t.TempDir(), "asn1.der")
+	os.WriteFile(f, buf, 0644)
+	out, err := exec.Command("openssl", "asn1parse", "-in", f, "-inform", "der", "-dump", "-i").Output()
+	if err != nil {
+		fmt.Println(string(out))
+		log.Fatal(err)
+	}
+	return out
+}
+
+func DiffText(t *testing.T, a, b []byte) {
+	aFile := path.Join(t.TempDir(), "a.der")
+	bFile := path.Join(t.TempDir(), "b.der")
+	os.WriteFile(aFile, a, 0644)
+	os.WriteFile(bFile, b, 0644)
+	out, _ := exec.Command("git", "diff", "--color=always", "--no-index", "--", aFile, bFile).Output()
+	if len(out) > 0 {
+		fmt.Println(string(a))
+		fmt.Println(string(b))
+		fmt.Println(string(out))
+	}
+}
+
+func Asn1Compare(t *testing.T, a, b []byte) {
+	a = Asn1Parse(t, a)
+	b = Asn1Parse(t, b)
+	DiffText(t, a, b)
 }
