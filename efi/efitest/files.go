b1127e50c45efbb4c44af14cf29e091a5871e290
Morten Linderud <morten@linderud.pw>
2024-01-07 15:37:12 +0100

    efitest: correct byteorder in the test files
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- efi/efitest/files.go
+++ efi/efitest/files.go
@@ -5,27 +5,27 @@ import "testing/fstest"
 func SecureBootOff() fstest.MapFS {
 	return fstest.MapFS{
 		"/sys/firmware/efi/efivars/SecureBoot-8be4df61-93ca-11d2-aa0d-00e098032b8c": {
-			Data: []byte{0x0, 0x6, 0x0, 0x0, 0x0}},
+			Data: []byte{0x6, 0x0, 0x0, 0x0, 0x0}},
 	}
 }
 
 func SecureBootOn() fstest.MapFS {
 	return fstest.MapFS{
 		"/sys/firmware/efi/efivars/SecureBoot-8be4df61-93ca-11d2-aa0d-00e098032b8c": {
-			Data: []byte{0x0, 0x6, 0x0, 0x0, 0x1}},
+			Data: []byte{0x6, 0x0, 0x0, 0x0, 0x1}},
 	}
 }
 
 func SetUpModeOn() fstest.MapFS {
 	return fstest.MapFS{
 		"/sys/firmware/efi/efivars/SetupMode-8be4df61-93ca-11d2-aa0d-00e098032b8c": {
-			Data: []byte{0x0, 0x6, 0x0, 0x0, 0x1}},
+			Data: []byte{0x6, 0x0, 0x0, 0x0, 0x1}},
 	}
 }
 
 func SetUpModeOff() fstest.MapFS {
 	return fstest.MapFS{
 		"/sys/firmware/efi/efivars/SetupMode-8be4df61-93ca-11d2-aa0d-00e098032b8c": {
-			Data: []byte{0x0, 0x6, 0x0, 0x0, 0x1}},
+			Data: []byte{0x6, 0x0, 0x0, 0x0, 0x1}},
 	}
 }
